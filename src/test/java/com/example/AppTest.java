9368a8223c194a61a40fa886b76d7dc6df07a582
Ubuntu <ubuntu@ip-172-31-47-0.ap-south-1.compute.internal>
2026-06-21 13:09:02 +0000

    localfile

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+import org.junit.Test;
+import org.junit.Assert;
+public class AppTest 
+{
+    public void testAdd()
+    {
+	App app=new App();
+	int result=app.add(2,3);
+	System.out.println("Running Test : 2+3="+result);
+        Assert.assertEquals( 5,result );
+    }
+}
